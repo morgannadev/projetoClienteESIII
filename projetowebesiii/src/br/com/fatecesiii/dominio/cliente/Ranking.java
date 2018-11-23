7703076460f312e9e09ab339fc30a1669291383b
Morganna Giovanelli De Souza <morgannasouza@brq.com>
2018-11-23 20:36:44 -0200

    Adjust getters and setters

--- projetowebesiii/src/br/com/fatecesiii/dominio/cliente/Ranking.java
+++ projetowebesiii/src/br/com/fatecesiii/dominio/cliente/Ranking.java
@@ -5,4 +5,12 @@ import br.com.fatecesiii.dominio.entidadedominio.EntidadeDominio;
 public class Ranking extends EntidadeDominio {
 
 	private String ranking;
+
+	public String getRanking() {
+		return ranking;
+	}
+
+	public void setRanking(String ranking) {
+		this.ranking = ranking;
+	}
 }
