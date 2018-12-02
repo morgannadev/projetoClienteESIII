74dcfb72ee58b0e3368f02f630c36a948379dc96
Morganna Giovanelli <morgcontato@gmail.com>
2018-12-02 00:01:18 -0200

    Update documents

--- documents/script_bancodedados.sql
+++ documents/script_bancodedados.sql
@@ -3,12 +3,6 @@
 
 
 
-CREATE TABLE TB_USUARIO (
-id_usuario Número(4) PRIMARY KEY,
-usuario varchar,
-senha varchar
-)
-
 CREATE TABLE TB_TIPO_RESIDENCIA (
 id_tipo_residencia Número(4) PRIMARY KEY,
 tipo_residencia varchar
@@ -19,16 +13,16 @@ id_tipo_logradouro Número(4) PRIMARY KEY,
 tipo_logradouro varchar
 )
 
-CREATE TABLE TB_PAIS (
-id_pais Número(4) PRIMARY KEY,
-pais varchar
+CREATE TABLE TB_BAIRRO (
+id_bairro Número(4) PRIMARY KEY,
+bairro varchar,
+id_cidade Número(4)
 )
 
 CREATE TABLE TB_ESTADO (
 id_estado Número(4) PRIMARY KEY,
 estado varchar,
-id_pais Número(4),
-FOREIGN KEY(id_pais) REFERENCES TB_PAIS (id_pais)
+id_pais Número(4)
 )
 
 CREATE TABLE TB_CIDADE (
@@ -38,11 +32,14 @@ id_estado Número(4),
 FOREIGN KEY(id_estado) REFERENCES TB_ESTADO (id_estado)
 )
 
-CREATE TABLE TB_BAIRRO (
-id_bairro Número(4) PRIMARY KEY,
-bairro varchar,
-id_cidade Número(4),
-FOREIGN KEY(id_cidade) REFERENCES TB_CIDADE (id_cidade)
+CREATE TABLE TB_PAIS (
+id_pais Número(4) PRIMARY KEY,
+pais varchar
+)
+
+CREATE TABLE TB_TIPO_ENDERECO (
+id_tipo_endereco Número(4) PRIMARY KEY,
+tipo_endereco varchar
 )
 
 CREATE TABLE TB_ENDERECO (
@@ -60,15 +57,11 @@ id_estado Número(4),
 id_pais Número(4),
 FOREIGN KEY(id_tipo_logradouro) REFERENCES TB_TIPO_LOGRADOURO (id_tipo_logradouro),
 FOREIGN KEY(id_tipo_residencia) REFERENCES TB_TIPO_RESIDENCIA (id_tipo_residencia),
+FOREIGN KEY(id_tipo_endereco) REFERENCES TB_TIPO_ENDERECO (id_tipo_endereco),
 FOREIGN KEY(id_bairro) REFERENCES TB_BAIRRO (id_bairro),
 FOREIGN KEY(id_cidade) REFERENCES TB_CIDADE (id_cidade),
 FOREIGN KEY(id_estado) REFERENCES TB_ESTADO (id_estado),
 FOREIGN KEY(id_pais) REFERENCES TB_PAIS (id_pais)
-)
-
-CREATE TABLE TB_TIPO_ENDERECO (
-id_tipo_endereco Número(4) PRIMARY KEY,
-tipo_endereco varchar
 )
 
 CREATE TABLE TB_CARTAO (
@@ -81,26 +74,14 @@ id_tipo_cartao Número(4),
 id_bandeira Número(4)
 )
 
-CREATE TABLE TB_TIPO_CARTAO (
-id_tipo_cartao Número(4) PRIMARY KEY,
-tipo_cartao varchar
-)
-
 CREATE TABLE TB_BANDEIRA (
 id_bandeira Número(4) PRIMARY KEY,
 bandeira varchar
 )
 
--- Erro: Nome de tabela duplicado (este erro compromete a integridade referencial)!
-CREATE TABLE TB_USUARIO (
-id_usuario Número(4) PRIMARY KEY,
-usuario varchar,
-senha varchar
-)
-
-CREATE TABLE TB_TRANSACAO (
-id_transacao Número(4) PRIMARY KEY,
-transacao varchar
+CREATE TABLE TB_TIPO_CARTAO (
+id_tipo_cartao Número(4) PRIMARY KEY,
+tipo_cartao varchar
 )
 
 CREATE TABLE TB_TELEFONE (
@@ -130,6 +111,17 @@ id_genero Número(4) PRIMARY KEY,
 genero varchar
 )
 
+CREATE TABLE TB_TRANSACAO (
+id_transacao Número(4) PRIMARY KEY,
+transacao varchar
+)
+
+CREATE TABLE TB_USUARIO (
+id_usuario Número(4) PRIMARY KEY,
+usuario varchar,
+senha varchar
+)
+
 CREATE TABLE TB_CLIENTE (
 id_cliente Número(4) PRIMARY KEY,
 nome varchar,
@@ -143,16 +135,19 @@ id_ranking Número(4),
 id_transacao Número(4),
 id_usuario Número(4),
 id_status_cliente Número(4),
+id_cartao Número(4),
 FOREIGN KEY(id_genero) REFERENCES TB_GENERO (id_genero),
 FOREIGN KEY(/*erro: ??*/) REFERENCES TB_TELEFONE (),
 FOREIGN KEY(id_endereco) REFERENCES TB_ENDERECO (id_endereco),
 FOREIGN KEY(id_ranking) REFERENCES TB_RANKING (id_ranking),
 FOREIGN KEY(id_transacao) REFERENCES TB_TRANSACAO (id_transacao),
 FOREIGN KEY(id_usuario) REFERENCES TB_USUARIO (id_usuario),
-FOREIGN KEY(id_status_cliente) REFERENCES TB_STATUS_CLIENTE (id_status_cliente)
+FOREIGN KEY(id_status_cliente) REFERENCES TB_STATUS_CLIENTE (id_status_cliente),
+FOREIGN KEY(id_cartao) REFERENCES TB_CARTAO (id_cartao)
 )
 
-ALTER TABLE TB_ENDERECO ADD FOREIGN KEY(id_tipo_endereco) REFERENCES TB_TIPO_ENDERECO (id_tipo_endereco)
+ALTER TABLE TB_BAIRRO ADD FOREIGN KEY(id_cidade) REFERENCES TB_CIDADE (id_cidade)
+ALTER TABLE TB_ESTADO ADD FOREIGN KEY(id_pais) REFERENCES TB_PAIS (id_pais)
 ALTER TABLE TB_CARTAO ADD FOREIGN KEY(id_tipo_cartao) REFERENCES TB_TIPO_CARTAO (id_tipo_cartao)
 ALTER TABLE TB_CARTAO ADD FOREIGN KEY(id_bandeira) REFERENCES TB_BANDEIRA (id_bandeira)
 ALTER TABLE TB_TELEFONE ADD FOREIGN KEY(id_tipo_telefone) REFERENCES TB_TIPO_TELEFONE (id_tipo_telefone)
