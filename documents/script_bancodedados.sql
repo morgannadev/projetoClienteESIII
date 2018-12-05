dbc7c91fb140c8639c8afe81c9f348cd1a2e42b0
Morganna Giovanelli De Souza <morgannasouza@brq.com>
2018-12-05 04:20:08 -0200

    Update database script

--- documents/script_bancodedados.sql
+++ documents/script_bancodedados.sql
@@ -4,35 +4,35 @@ USE projetoweb;
 
 CREATE TABLE IF NOT EXISTS TB_TIPO_LOGRADOURO (
     id_tipo_logradouro INT(4) AUTO_INCREMENT,
-    tipo_logradouro VARCHAR(255) NOT NULL,
-	data_cadastro DATETIME NOT NULL,
+    tipo_logradouro VARCHAR(255),
+	data_cadastro DATETIME,
     PRIMARY KEY (id_tipo_logradouro)
 );
 
 CREATE TABLE IF NOT EXISTS TB_TIPO_ENDERECO (
 	id_tipo_endereco INT(4) AUTO_INCREMENT,
-	tipo_endereco VARCHAR(255) NOT NULL,
-	data_cadastro DATETIME NOT NULL,
+	tipo_endereco VARCHAR(255),
+	data_cadastro DATETIME,
 	PRIMARY KEY (id_tipo_endereco)
 );
 
 CREATE TABLE IF NOT EXISTS TB_TIPO_RESIDENCIA (
 	id_tipo_residencia INT(4) AUTO_INCREMENT,
-	tipo_residencia VARCHAR(255) NOT NULL,
-	data_cadastro DATETIME NOT NULL,
+	tipo_residencia VARCHAR(255),
+	data_cadastro DATETIME,
 	PRIMARY KEY (id_tipo_residencia)
 );
 
 CREATE TABLE IF NOT EXISTS TB_PAIS (
 	id_pais INT(4) AUTO_INCREMENT,
-	pais VARCHAR(255) NOT NULL,
-	data_cadastro DATETIME NOT NULL,
+	pais VARCHAR(255),
+	data_cadastro DATETIME,
 	PRIMARY KEY (id_pais)
 );
 
 CREATE TABLE IF NOT EXISTS TB_ESTADO (
 	id_estado INT(4) AUTO_INCREMENT,
-	estado VARCHAR(255) NOT NULL,
+	estado VARCHAR(255),
 	id_pais INT(4),
 	data_cadastro DATETIME,
 	PRIMARY KEY (id_estado),
@@ -41,29 +41,29 @@ CREATE TABLE IF NOT EXISTS TB_ESTADO (
 
 CREATE TABLE IF NOT EXISTS TB_CIDADE (
 	id_cidade INT(4) AUTO_INCREMENT,
-	cidade VARCHAR(100) NOT NULL,
+	cidade VARCHAR(100),
 	id_estado INT(4),
-	data_cadastro DATETIME NOT NULL,
+	data_cadastro DATETIME,
 	PRIMARY KEY (id_cidade),
 	FOREIGN KEY(id_estado) REFERENCES TB_ESTADO (id_estado)
 );
 
 CREATE TABLE IF NOT EXISTS TB_BAIRRO (
 	id_bairro INT(4) AUTO_INCREMENT,
-	bairro VARCHAR(100) NOT NULL,
+	bairro VARCHAR(100),
 	id_cidade INT(4),
-	data_cadastro DATETIME NOT NULL,
+	data_cadastro DATETIME,
 	PRIMARY KEY (id_bairro),
 	FOREIGN KEY(id_cidade) REFERENCES TB_CIDADE (id_cidade)
 );
 
 CREATE TABLE IF NOT EXISTS TB_ENDERECO (
 	id_endereco INT(4) AUTO_INCREMENT,
-	logradouro VARCHAR(100) NOT NULL,
-	numero INT(6) NOT NULL,
-	cep VARCHAR(8) NOT NULL,
-	observacoes VARCHAR(255) NOT NULL,
-	data_cadastro DATETIME NOT NULL,
+	logradouro VARCHAR(100),
+	numero INT(6),
+	cep VARCHAR(8),
+	observacoes VARCHAR(255),
+	data_cadastro DATETIME,
 	id_tipo_logradouro INT(4),
 	id_tipo_residencia INT(4),
 	id_tipo_endereco INT(4),
@@ -83,27 +83,27 @@ CREATE TABLE IF NOT EXISTS TB_ENDERECO (
 
 CREATE TABLE IF NOT EXISTS TB_TIPO_CARTAO (
 	id_tipo_cartao INT(4) AUTO_INCREMENT,
-	tipo_cartao VARCHAR(20) NOT NULL,
-	data_cadastro DATETIME NOT NULL,
+	tipo_cartao VARCHAR(20),
+	data_cadastro DATETIME,
 	PRIMARY KEY (id_tipo_cartao)
 );
 
 CREATE TABLE IF NOT EXISTS TB_BANDEIRA (
 	id_bandeira INT(4) AUTO_INCREMENT,
-	bandeira VARCHAR(30) NOT NULL,
-	data_cadastro DATETIME NOT NULL,
+	bandeira VARCHAR(30),
+	data_cadastro DATETIME,
 	PRIMARY KEY (id_bandeira)
 );
 
 CREATE TABLE IF NOT EXISTS TB_CARTAO (
 	id_cartao INT(4) AUTO_INCREMENT,
-	cartao VARCHAR(255) NOT NULL,
-	is_preferencial BOOLEAN NOT NULL,
-	numero_cartao VARCHAR(16) NOT NULL,
+	cartao VARCHAR(255),
+	is_preferencial BOOLEAN,
+	numero_cartao VARCHAR(16),
 	codigo_seguranca INT(3),
 	id_tipo_cartao INT(4),
 	id_bandeira INT(4),
-	data_cadastro DATETIME NOT NULL,
+	data_cadastro DATETIME,
 	PRIMARY KEY (id_cartao),
 	FOREIGN KEY(id_tipo_cartao) REFERENCES TB_TIPO_CARTAO (id_tipo_cartao),
 	FOREIGN KEY(id_bandeira) REFERENCES TB_BANDEIRA (id_bandeira)
@@ -111,63 +111,63 @@ CREATE TABLE IF NOT EXISTS TB_CARTAO (
 
 CREATE TABLE IF NOT EXISTS TB_STATUS_CLIENTE (
 	id_status_cliente INT(4) AUTO_INCREMENT,
-	status_cliente VARCHAR(15) NOT NULL,
-	data_cadastro DATETIME NOT NULL,
+	status_cliente VARCHAR(15),
+	data_cadastro DATETIME,
 	PRIMARY KEY (id_status_cliente)
 );
 
 CREATE TABLE IF NOT EXISTS TB_RANKING (
 	id_ranking INT(4) AUTO_INCREMENT,
-	ranking INT(4) NOT NULL,
-	data_cadastro DATETIME NOT NULL,
+	ranking INT(4),
+	data_cadastro DATETIME,
 	PRIMARY KEY (id_ranking)
 );
 
 CREATE TABLE IF NOT EXISTS TB_TIPO_TELEFONE (
 	id_tipo_telefone INT(4) AUTO_INCREMENT,
-	tipo_telefone VARCHAR(15) NOT NULL,
-	data_cadastro DATETIME NOT NULL,
+	tipo_telefone VARCHAR(15),
+	data_cadastro DATETIME,
 	PRIMARY KEY (id_tipo_telefone)
 );
 
 CREATE TABLE IF NOT EXISTS TB_TELEFONE (
 	id_telefone INT(4) AUTO_INCREMENT,
-	ddd INT(2) NOT NULL,
-	telefone VARCHAR(10) NOT NULL,
+	ddd INT(2),
+	telefone VARCHAR(10),
 	id_tipo_telefone INT(4),
-	data_cadastro DATETIME NOT NULL,
+	data_cadastro DATETIME,
 	PRIMARY KEY (id_telefone),
 	FOREIGN KEY(id_tipo_telefone) REFERENCES TB_TIPO_TELEFONE (id_tipo_telefone)
 );
 
 CREATE TABLE IF NOT EXISTS TB_TRANSACAO (
 	id_transacao INT(4) AUTO_INCREMENT,
-	transacao VARCHAR(255) NOT NULL,
-	data_cadastro DATETIME NOT NULL,
+	transacao VARCHAR(255),
+	data_cadastro DATETIME,
 	PRIMARY KEY (id_transacao)
 );
 
 CREATE TABLE IF NOT EXISTS TB_USUARIO (
 	id_usuario INT(4) AUTO_INCREMENT,
-	usuario VARCHAR(20) NOT NULL,
-	senha VARCHAR(20) NOT NULL,
-	data_cadastro DATETIME NOT NULL,
+	usuario VARCHAR(20),
+	senha VARCHAR(20),
+	data_cadastro DATETIME,
 	PRIMARY KEY (id_usuario)
 );
 
 CREATE TABLE IF NOT EXISTS TB_GENERO (
 	id_genero INT(4) AUTO_INCREMENT,
-	genero VARCHAR(30) NOT NULL,
-	data_cadastro DATETIME NOT NULL, 
+	genero VARCHAR(30),
+	data_cadastro DATETIME, 
 	PRIMARY KEY (id_genero)
 );
 
 CREATE TABLE IF NOT EXISTS TB_CLIENTE (
 	id_cliente INT(4) AUTO_INCREMENT,
-	nome VARCHAR(255) NOT NULL,
-	cpf VARCHAR(11) NOT NULL,
-	data_nascimento DATETIME NOT NULL,
-	email VARCHAR(255) NOT NULL,
+	nome VARCHAR(255),
+	cpf VARCHAR(11),
+	data_nascimento DATETIME,
+	email VARCHAR(255),
 	id_genero INT(4),
 	id_endereco INT(4),
 	id_ranking INT(4),
@@ -176,7 +176,7 @@ CREATE TABLE IF NOT EXISTS TB_CLIENTE (
 	id_status_cliente INT(4),
 	id_cartao INT(4),
 	id_telefone INT(4),
-	data_cadastro DATETIME NOT NULL,
+	data_cadastro DATETIME,
 	PRIMARY KEY (id_cliente),
 	FOREIGN KEY(id_genero) REFERENCES TB_GENERO (id_genero),
 	FOREIGN KEY(id_endereco) REFERENCES TB_ENDERECO (id_endereco),
@@ -187,3 +187,7 @@ CREATE TABLE IF NOT EXISTS TB_CLIENTE (
 	FOREIGN KEY(id_cartao) REFERENCES TB_CARTAO (id_cartao),
 	FOREIGN KEY(id_telefone) REFERENCES TB_TELEFONE (id_telefone)
 );
+
+INSERT INTO TB_GENERO (genero, data_cadastro) VALUES ('Feminino', now());
+INSERT INTO TB_GENERO (genero, data_cadastro) VALUES ('Masculino', now());
+INSERT INTO TB_GENERO (genero, data_cadastro) VALUES ('Outros', now());
